0fc039309818299cb0e48886b4841e43f9df0413
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-08-03 11:34:41 +0300

    Added bugs.

--- src/ui/settings.py
+++ src/ui/settings.py
@@ -1,14 +1,21 @@
-from supervisely.app.widgets import Text, Card
+from supervisely.app.widgets import Text, Card, Button, Container
 
 example_text = Text("This is a widget for settings.", "info")
+
+raise_error_button = Button("Raise error")
+
+
+@raise_error_button.click
+def raise_error():
+    raise ValueError("test")
 
 
 card = Card(
     title="2️⃣ Settings",
     description="PLACEHOLDER: Input description here.",
-    content=example_text,
+    content=Container([example_text, raise_error_button]),
     lock_message="Select the dataset on step 1️⃣.",
     collapsable=True,
 )
-card.lock()
-card.collapse()
+# card.lock()
+# card.collapse()
